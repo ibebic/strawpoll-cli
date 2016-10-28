473e96256f29c56c2661bf568fdabb310d8c8444
ibebic <igor.bebic@gmail.com>
2016-10-28 10:51:46 +0200

    Added ora spinner during loading

--- options.js
+++ options.js
@@ -4,6 +4,8 @@ const inquirer = require('inquirer');
 const request = require('request');
 const chalk = require('chalk');
 const open = require('opn');
+const ora = require('ora');
+const spinner = ora('Loading');
 
   let output = [];
   let additionals = [
@@ -26,6 +28,8 @@ function addOptions(originals) {
     if (answers.askAgain) {
       addOptions(originals);
     } else {
+      spinner.color = 'white';
+      spinner.start();
       let formattedReq = {'title': originals.title, 'options': output, 'multi': originals.multi};
       request({
         url: 'https://strawpoll.me/api/v2/polls',
@@ -33,8 +37,11 @@ function addOptions(originals) {
         followAllRedirects: true,
         json: formattedReq
       }, function(error, response, body) {
-        console.log('\nPoll created at http://www.strawpoll.me/' + chalk.bold.green(body.id));
+        spinner.text = 'Done';
+        spinner.succeed();
+        console.log('Poll created at http://www.strawpoll.me/' + chalk.bold.green(body.id));
         open('http://www.strawpoll.me/' + body.id);
+        process.exit();
       });
     }
   });
--- showResults.js
+++ showResults.js
@@ -4,18 +4,24 @@ const EventEmitter = require('events').EventEmitter;
 const request = require('request');
 const Pie = require('cli-pie');
 const chalk = require('chalk');
+const ora = require('ora');
+const spinner = ora('Loading');
 
 function showResults(pollAddress) {
+  spinner.color = 'white';
+  spinner.start();
   let body = new EventEmitter();
   request.get(pollAddress, function (error, response, data) {
     body.data = data;
     body.emit('update');
   });
   body.on('update', function () {
+    spinner.text = 'Done';
+    spinner.succeed();
     let obj = JSON.parse(body.data);
     let pollOptions = obj.options.length;
     let totalVotes = 0;
-    console.log(chalk.bold('\nPoll subject:\n\n') + obj.title + '\n' + chalk.bold('\n-----RESULTS-----\n'));
+    console.log(chalk.bold('Poll subject:\n\n') + obj.title + '\n' + chalk.bold('\n-----RESULTS-----\n'));
     let resultPie = new Pie(12, [], { legend: true });
     for (let i = 0; i < pollOptions; i++) {
       totalVotes += obj.votes[i];
